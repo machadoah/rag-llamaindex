bec79f418e2cb31d546e64b2b299b4f66ae1b13d
Isaah <isaahs41@gmail.com>
2024-09-17 13:57:37 -0300

    feat: Inserindo logs na ingestão e querying

--- rag.py
+++ rag.py
@@ -1,3 +1,4 @@
+from llama_index.core.base.base_query_engine import BaseQueryEngine
 from llama_index.core.readers.file.base import SimpleDirectoryReader
 
 # Responsável por ler documentos de um diretório e prepará-los para serem indexados.
@@ -30,10 +31,12 @@ def ingest() -> VectorStoreIndex:
     # Salva o índice em um arquivo
     index.storage_context.persist(persist_dir="index")
 
+    logger.info("Índice criado com sucesso!")
+
     return index
 
 
-def querying(index: VectorStoreIndex):
+def querying(index: VectorStoreIndex) -> BaseQueryEngine:
     """
     Cria um motor de consulta (QueryEngine) a partir do índice fornecido.
 
@@ -41,19 +44,21 @@ def querying(index: VectorStoreIndex):
         index (VectorStoreIndex): O índice a ser usado pelo motor de consulta.
 
     Returns:
-        QueryEngine: O motor de consulta configurado.
+        BaseQueryEngine: O motor de consulta configurado.
     """
 
-    # Cria o motor de consulta (QueryEngine)
-    query_engine = index.as_query_engine()
+    # Retorna o motor de consulta (QueryEngine)
 
-    return query_engine
+    logger.info("Motor de consulta criado com sucesso!")
+
+    return index.as_query_engine()
 
 
 if __name__ == "__main__":
     index = ingest()
 
-    logger.info("""
+    print(
+        """
     Olá, bem vindo ao assistente da Fatec!
     
     Você pode perguntar sobre informações relacionados ao curso ou a diciplina de TEI.
@@ -61,18 +66,30 @@ if __name__ == "__main__":
     Caso queira sair digite algo como exit, quit ou 0!
     
     Obrigado!
-    """)
-
-
+    """
+    )
 
     query_engine = querying(index)
 
     while True:
         pergunta = input("Insira sua pergunta: ")
 
-        if pergunta in ['q', 'quit', 'exit', 'quit()', 'exit()', 0]:
+        if pergunta.lower() in [
+            "q",
+            "quit",
+            "exit",
+            "quit()",
+            "exit()",
+            "0",
+            "sair",
+            "parar",
+            "stop",
+        ]:
             break
 
         # Realiza uma consulta usando o motor de consulta
         r = query_engine.query(pergunta)
+
+        logger.info("Pesquisando na base de dados...")
+
         print(r)
